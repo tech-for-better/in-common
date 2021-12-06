b39b71a745816a1f1b60cfe5d7d79be522a7822e
bubbles linux <appenzeller.barbara@gmail.com>
2021-12-06 16:36:06 +0000

    Airtable practice. Access update delete, display on page
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

---
+++ pages/airtable-test.js
@@ -0,0 +1,38 @@
+import {
+  tableSchedule,
+  getSchedule,
+  miniRecord,
+  createRecord,
+  updateRecord,
+  getIds,
+  updateThisRecord,
+  deleteRecord,
+} from "../lib/model";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+
+const getRecords = async () => {
+  const records = await getSchedule();
+  return records;
+};
+
+getRecords();
+
+export default function Event() {
+  const [records, setRecords] = useState([]);
+  useEffect(() => {
+    getRecords().then((data) => setRecords(data));
+  }, []);
+  console.log("useState Records: ", records);
+
+  return (
+    <>
+      <h1>Hello</h1>
+      {records.map((record) => (
+        <p>
+          Activity: {record.fields.Activity}, Status: {record.fields.Status}
+        </p>
+      ))}
+    </>
+  );
+}
