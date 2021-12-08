02debd09582c4db5e196fb1af5c9dacbd5a706b9
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-08 12:28:37 +0000

    we were able to display if a user is logged in our not
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- conponents/SignUpComponent.js
+++ conponents/SignUpComponent.js
@@ -31,68 +31,89 @@ export default function SignUpComponent() {
         signUpEmail,
         signUpPassword
       );
+
+      alert('you are signed up');
     } catch (error) {
       console.log(error);
+      alert('sorry we could not sign you up');
     }
   }
 
+  //   async function logOut() {
+  //     try {
+  //       await signOut(auth);
+  //       console.log('signed out!');
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+
+  //   console.log('logOut value -->', logOut());
+
   return (
-    <Container component="main" maxWidth="xs">
-      <Card
-        sx={{
-          minWidth: 275,
-          marginTop: 8,
-          padding: 5,
-          borderRadius: 3,
+    <>
+      {user
+        ? console.log(
+            'you are signed out already (return of the SignUpComponent)'
+          )
+        : console.log('did not sign in')}
+      <Container component="main" maxWidth="xs">
+        <Card
+          sx={{
+            minWidth: 275,
+            marginTop: 8,
+            padding: 5,
+            borderRadius: 3,
 
-          mt: 5,
-        }}
-      >
-        <form>
-          <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
-            Sign up
-          </Typography>
+            mt: 5,
+          }}
+        >
+          <form>
+            <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
+              Sign up
+            </Typography>
 
-          <Box sx={{ mb: 2 }}>
-            <label hidden htmlFor="email">
-              Email
-            </label>
-            <TextField
+            <Box sx={{ mb: 2 }}>
+              <label hidden htmlFor="email">
+                Email
+              </label>
+              <TextField
+                variant="outlined"
+                id="filled-email-input"
+                label="Email"
+                type="email"
+                autoComplete="current-email"
+                onChange={(e) => setSignUpEmail(e.target.value)}
+                fullWidth
+              />
+            </Box>
+            <Box sx={{ mb: 2 }}>
+              <label hidden htmlFor="password">
+                Password
+              </label>
+              <TextField
+                id="filled-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                onChange={(e) => setSignUpPassword(e.target.value)}
+                fullWidth
+              />
+            </Box>
+            <Button
               variant="outlined"
-              id="filled-email-input"
-              label="Email"
-              type="email"
-              autoComplete="current-email"
-              onChange={(e) => setSignUpEmail(e.target.value)}
+              sx={{ padding: 1.85 }}
+              onClick={(e) => {
+                e.preventDefault();
+                signUp();
+              }}
               fullWidth
-            />
-          </Box>
-          <Box sx={{ mb: 2 }}>
-            <label hidden htmlFor="password">
-              Password
-            </label>
-            <TextField
-              id="filled-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              onChange={(e) => setSignUpPassword(e.target.value)}
-              fullWidth
-            />
-          </Box>
-          <Button
-            variant="outlined"
-            sx={{ padding: 1.85 }}
-            onClick={(e) => {
-              e.preventDefault();
-              signUp();
-            }}
-            fullWidth
-          >
-            Sign up
-          </Button>
-        </form>
-      </Card>
-    </Container>
+            >
+              Sign up
+            </Button>
+          </form>
+        </Card>
+      </Container>
+    </>
   );
 }
