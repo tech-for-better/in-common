8d5bcb68198af0043c1150a67fd04e1e2d6daa58
aaadriana <adriana@stanciu.uk>
2021-12-08 16:42:53 +0000

    Added new inputs in the Signup form
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- components/Signup/Confirmation.js
+++ components/Signup/Confirmation.js
@@ -1,26 +1,9 @@
 import { Container, Box, Alert } from '@mui/material';
 import { auth } from '../../firebase';
 import { signOut } from '@firebase/auth';
-// import { updateUser } from '@firebase/auth';
 
 export default function Confirmation({ user, setUser }) {
-  //     getAuth()
-  //     .updateUser(uid, {
-  //       email: 'modifiedUser@example.com',
-  //       phoneNumber: '+11234567890',
-  //       emailVerified: true,
-  //       password: 'newPassword',
-  //       displayName: 'Jane Doe',
-  //       photoURL: 'http://www.example.com/12345678/photo.png',
-  //       disabled: true,
-  //     })
-  //     .then((userRecord) => {
-  //       // See the UserRecord reference doc for the contents of userRecord.
-  //       console.log('Successfully updated user', userRecord.toJSON());
-  //     })
-  //     .catch((error) => {
-  //       console.log('Error updating user:', error);
-  //     });
+
 
   async function logOut() {
     try {
