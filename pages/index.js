cbde3dc622d7c76d6751b4bfad2743cfe0fe3094
Cemal Okten <cemalokten@Cemals-MacBook-Air.local>
2021-12-08 14:19:43 +0000

    Login component with 'User' state from _App.js
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,6 @@
 import * as React from "react";
-import { Card } from "@mui/material";
-import { TextField } from "@mui/material";
-import { Container, Box } from "@mui/material";
-import { Button } from "@mui/material";
+import Login from "../components/Login/Login";
 
-export default function Index() {
-  return (
-    <Container component="main" maxWidth="xs">
-      <Card
-        sx={{
-          minWidth: 275,
-          marginTop: 8,
-          padding: 5,
-          borderRadius: 3,
-          bgcolor: "#EBFFFA",
-          mt: 5,
-        }}
-      >
-        <Box sx={{ mb: 2 }}>
-          <TextField
-            variant="outlined"
-            id="filled-email-input"
-            label="Email"
-            type="email"
-            autoComplete="current-email"
-            fullWidth
-            error
-          />
-        </Box>
-        <Box sx={{ mb: 2 }}>
-          <TextField
-            sx={{ bgcolor: "#C8F4EC" }}
-            id="filled-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            fullWidth
-          />
-        </Box>
-        <Button variant="outlined" sx={{ padding: 1.6 }} fullWidth>
-          Login
-        </Button>
-      </Card>
-    </Container>
-  );
+export default function Index(props) {
+  return <Login {...props}></Login>;
 }
