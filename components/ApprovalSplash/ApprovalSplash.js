191e025f8c6ad9de998d148a77306e6a13e09ad1
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-12-08 17:00:37 +0000

    replaced splash message with template literal
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

--- components/ApprovalSplash/ApprovalSplash.js
+++ components/ApprovalSplash/ApprovalSplash.js
@@ -1,7 +1,7 @@
 export default function ApprovalSplash() {
   return (
     <p>
-      Approval pending, we'll email you once your account has been approved!
+      {`Approval pending, we'll email you once your account has been approved!`}
     </p>
   );
 }
