64a83a436030d176c5a41575a40b7d0169774bab
Cemal Okten <cemalokten@Cemals-MacBook-Air.local>
2021-12-09 11:36:19 +0000

    Fixed error with `if(record)` in _app.js
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import "../styles/globals.css";
-import { useState, useEffect } from "react";
-import { auth } from "../firebase";
-import { onAuthStateChanged } from "@firebase/auth";
-import { base } from "../lib/init-airtable";
+import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import { auth } from '../firebase';
+import { onAuthStateChanged } from '@firebase/auth';
+import { base } from '../lib/init-airtable';
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState({});
@@ -11,13 +11,13 @@ function MyApp({ Component, pageProps }) {
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
     if (currentUser) {
-      base("Accounts")
+      base('Accounts')
         .select({
           filterByFormula: `UID = "${currentUser.uid}"`,
         })
         .firstPage((err, records) => {
-          if (err) return console.log("Airtable error :", err);
-          setApproved(records[0].fields.Approval);
+          if (err) return console.log('Airtable error :', err);
+          if (records) return setApproved(records[0].fields.Approval);
         });
     }
   });
---
+++ pages/events.js
@@ -0,0 +1,7 @@
+import EventActivity from '../components/EventCreation/EventActivity';
+import EventDate from '../components/EventCreation/EventDate';
+import EventLocation from '../components/EventCreation/EventLocation';
+
+export default function events({ user }) {
+  return <EventLocation />;
+}
