f37a25bf3b3f04fc55e6f9be2d54a4b0b67b0c74
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-09 17:02:29 +0000

    able to display all airtable events
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- components/Events/Events.js
+++ components/Events/Events.js
@@ -1,43 +1,137 @@
 import { base } from '../../lib/init-airtable';
-import { Container, Box, Alert } from '@mui/material';
+import {
+  Container,
+  Box,
+  Alert,
+  Card,
+  InputLabel,
+  FormControl,
+  MenuItem,
+  Select,
+} from '@mui/material';
 import { useState, useEffect } from 'react';
 
 const tableEvents = base('Events');
 
 const getEvents = async () => {
   return await tableEvents
-    .select({ maxRecords: 3, view: 'Grid view' })
+    .select({
+      view: 'Grid view',
+    })
     .firstPage();
 };
 
-const getRecords = async () => {
-  const records = await getEvents();
+const getRecords = async (callback) => {
+  const records = await callback;
   return records;
 };
 
-getRecords();
+// getRecords(getEvents());
+
+const getEventUidStatus = async (senderUID, status) => {
+  return await tableEvents
+    .select({
+      view: 'Grid view',
+      filterByFormula: `AND({Sender UID} = ${senderUID}, {Status} = ${status})`,
+    })
+    .firstPage();
+};
+
+getRecords(getEventUidStatus(1, `"Sent invite"`)).then((data) =>
+  console.log('get my records', data)
+);
+
+const getMyPartnerUID = async (myUID) => {
+  return await base('Accounts')
+    .select({
+      view: 'Grid view',
+      fields: [''],
+      filterByFormula: `{UID} = ${myUID}`,
+    })
+    .firstPage();
+};
+getRecords(getMyPartnerUID(1)).then((data) =>
+  console.log('get my partner UID', data)
+);
 
 // getEvents();
-console.log('outside Component', getRecords());
+// console.log('outside Component', getRecords());
 
 export default function Events({ user }) {
   const [myEvents, setMyEvents] = useState([]);
+  const [date, setDate] = useState();
 
   useEffect(() => {
-    getRecords().then((data) => setMyEvents(data));
+    getRecords(getEvents()).then((data) => setMyEvents(data));
   }, []);
 
-  console.log({ myEvents });
+  // console.log({ myEvents });
 
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>All Events</h1>
       {myEvents.map((record) => (
         <Container maxWidth="xs" key={record.id}>
-          <div>
-            Activity: {record.fields.Activity}, Status: {record.fields.Status},
-            Recipient: {record.fields.Recipient}, sender: {record.fields.Sender}
-          </div>
+          <Card
+            sx={{
+              minWidth: 275,
+              marginTop: 8,
+              padding: 5,
+              borderRadius: 3,
+              backgroundColor: '#bbc3c7',
+              color: 'white',
+              mt: 5,
+            }}
+          >
+            <Card> Activity: {record.fields.Activity}</Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              {' '}
+              Status: {record.fields.Status}
+            </Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              {' '}
+              Recipient: {record.fields.Recipient}
+            </Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              {' '}
+              Sender: {record.fields.Sender}
+            </Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              Date Suggestions: {record.fields['Date Suggestions']}{' '}
+            </Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              {' '}
+              Duration: {record.fields.Duration}
+            </Card>
+            <Card
+              sx={{
+                marginTop: 2,
+              }}
+            >
+              {' '}
+              Group Size {record.fields['Group size']}
+            </Card>
+          </Card>
         </Container>
       ))}
     </div>
--- pages/events.js
+++ pages/events.js
@@ -1,9 +1,6 @@
 import Events from '../components/Events/Events';
+import Login from '../components/Login/Login';
 
 export default function EventsPage({ user }) {
-  return (
-    <>
-      <Events user={user} />{' '}
-    </>
-  );
+  return <>{user?.email ? <Events user={user} /> : <Login />}</>;
 }
