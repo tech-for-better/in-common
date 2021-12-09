5787345455664786783f3284c8eba2c8b6485a53
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-12-09 16:24:02 +0000

    event summary page now displays event summary

--- components/EventCreation/EventSummary.jsx
+++ components/EventCreation/EventSummary.jsx
@@ -1,16 +1,16 @@
 import { Button } from '@mui/material';
 
-export default function EventSummary({
-  stage,
-  setStage,
-  newEvent,
-  setNewEvent,
-}) {
+export default function EventSummary({ stage, setStage, newEvent }) {
   console.log({ newEvent });
   return (
     <>
       <p>Event Summary</p>
       <p>{`Step ${stage} of 4`}</p>
+      <p>{`${newEvent.activity} at ${newEvent.location}`}</p>
+      <p>Proposed dates:</p>
+      {newEvent.date.map((date, i) => (
+        <p key={i}>{`${date}`}</p>
+      ))}
       <Button variant="outlined" onClick={() => setStage(stage - 1)}>
         Back
       </Button>
