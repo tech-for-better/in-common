aa6672580ae03dac06be4e2c65a428555ee07aba
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-09 15:27:57 +0000

    we were able to get information from the air table
    Barbara — 12/06/2021
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- components/Events/Events.js
+++ components/Events/Events.js
@@ -1,9 +1,41 @@
+import { base } from '../../lib/init-airtable';
+import { useState, useEffect } from 'react';
+
+const tableEvents = base('Events');
+
+const getEvents = async () => {
+  return await tableEvents
+    .select({ maxRecords: 3, view: 'Grid view' })
+    .firstPage();
+};
+
+const getRecords = async () => {
+  const records = await getEvents();
+  return records;
+};
+
+getRecords();
+
+// getEvents();
+console.log('outside Component', getRecords());
+
 export default function Events({ user }) {
+  const [myEvents, setMyEvents] = useState([]);
+
+  useEffect(() => {
+    getRecords().then((data) => setMyEvents(data));
+  }, []);
+
+  console.log({ myEvents });
+
   return (
-    <p>
-      {' '}
-      Welcome to the Events
-      {/* {user?.email} */}
-    </p>
+    <div>
+      <h1>Hello</h1>
+      {myEvents.map((record) => (
+        <p key={record.id}>
+          Activity: {record.fields.Activity}, Status: {record.fields.Status}
+        </p>
+      ))}
+    </div>
   );
 }
