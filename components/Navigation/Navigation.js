988cb72e1c4f84643e681df6dc3ef0728ec3b857
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-13 16:40:05 +0000

    DashBoard completed

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -8,9 +8,15 @@ const Navigation = () => {
     <Container
       component="main"
       maxWidth="xs"
-      style={{ background: '#EBEFFF', color: 'white' }}
+      style={{ background: 'white', color: 'white' }}
+      sx={{
+        minWidth: 275,
+        padding: 5,
+        borderRadius: 3,
+        mt: 5,
+      }}
     >
-      <Card sx={{ mb: 3, padding: 5 }}>
+      <Card sx={{ mb: 3, padding: 5, background: '#EBEFFF' }}>
         <h2 style={{ color: '#36426C' }}>Control Panel</h2>
         <p style={{ color: '#36426C' }}>
           What would you like to do? Use the options below to organise an event
@@ -23,21 +29,21 @@ const Navigation = () => {
         </IconButton>
       </Card>
 
-      <Card sx={{ mb: 3 }}>
+      <Card sx={{ mb: 3, background: '#EBFFFA' }}>
         <IconButton>
-          <Link href="/">create event</Link>
+          <Link href="/create-event">Create event</Link>
         </IconButton>
       </Card>
 
-      <Card sx={{ mb: 3 }}>
+      <Card sx={{ mb: 3, background: '#EBF9FF' }}>
         <IconButton>
           <Link href="/events"> Mange Events</Link>
         </IconButton>
       </Card>
 
-      <Card sx={{ mb: 3 }}>
+      <Card sx={{ mb: 3, background: '#EBEFFF' }}>
         <IconButton>
-          <Link href="*"> Edit Profile</Link>
+          <Link href="/"> Edit Profile</Link>
         </IconButton>
       </Card>
     </Container>
