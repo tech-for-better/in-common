11d32a36c1b62af767b810822c42c034e48b136f
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-12-13 16:39:33 +0000

    removed console logs

--- pages/_app.js
+++ pages/_app.js
@@ -8,18 +8,14 @@ function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null);
   const [approved, setApproved] = useState(false);
 
-  console.log(approved);
-
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
   });
 
   useEffect(() => {
     if (user) {
-      console.log('use effect uid: ', user.uid);
       (async () => {
         try {
-          console.log('airbase approval fetch');
           base('Accounts')
             .select({
               filterByFormula: `UID = "${user.uid}"`,
