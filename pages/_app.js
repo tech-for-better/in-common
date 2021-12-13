7b16aaff8df8e864e9905c1bab03fb9108241126
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-12-13 15:21:16 +0000

    approval fetch working

--- pages/_app.js
+++ pages/_app.js
@@ -5,22 +5,37 @@ import { onAuthStateChanged } from '@firebase/auth';
 import { base } from '../lib/init-airtable';
 
 function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [approved, setApproved] = useState(false);
+
+  console.log(approved);
 
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
-    // if (currentUser) {
-    //   base('Accounts')
-    //     .select({
-    //       filterByFormula: `UID = "${currentUser.uid}"`,
-    //     })
-    //     .firstPage((err, records) => {
-    //       if (err) return console.log('Airtable error :', err);
-    //       if (records) return setApproved(records[0].fields.Approval);
-    //     });
-    // }
   });
+
+  useEffect(() => {
+    if (user) {
+      console.log('use effect uid: ', user.uid);
+      (async () => {
+        try {
+          console.log('airbase approval fetch');
+          base('Accounts')
+            .select({
+              filterByFormula: `UID = "${user.uid}"`,
+            })
+            .firstPage((err, records) => {
+              if (err) return console.log('Airtable error :', err);
+              if (records) {
+                setApproved(records[0].fields.Approval);
+              }
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }, [user]);
 
   return <Component {...pageProps} user={user} approved={approved} />;
 }
