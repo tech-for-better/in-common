529c3d13af2796260fa00e9b7b50acecca6c8b6b
cemalokten <cemalokten@gmail.com>
2021-12-16 00:04:27 +0000

    Loads of work for sad face

--- components/Signup/SignUpComponent.js
+++ components/Signup/SignUpComponent.js
@@ -1,6 +1,6 @@
 import { auth } from '../../firebase';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
   Card,
   TextField,
@@ -13,23 +13,43 @@ import {
   InputLabel,
   FormControl,
   Alert,
+  Stack,
 } from '@mui/material';
-import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
-import { GridColumnHeaderSeparator } from '@mui/x-data-grid';
-import { ConstructionOutlined } from '@mui/icons-material';
 
-export default function SignUpComponent({ user }) {
+import Error from '../Alert/Error';
+
+export default function SignUpComponent() {
   const [signUpEmail, setSignUpEmail] = useState('');
   const [signUpPassword, setSignUpPassword] = useState('');
-  const [type, setType] = useState('');
+  const [orgType, setOrgType] = useState('');
   const [orgName, setOrgName] = useState('');
   const [contactEmail, setContactEmail] = useState('');
   const [contactName, setContactName] = useState('');
   const [contactNumber, setContactNumber] = useState('');
-
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState({});
+  const [counter, setCounter] = useState(false);
+  const [error, setError] = useState(true);
+  const isMounted = useRef(false);
   // const [loading, setLoading] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState({
+    EMAIL: false,
+    PASSWORD: false,
+    ORG_TYPE: false,
+    ORG_NAME: false,
+    CONTACT_EMAIL: false,
+    CONTACT_NAME: false,
+    CONTACT_NUMBER: false,
+  });
+
+  useEffect(() => {
+    if (isMounted.current) {
+      if (Object.values(errorMessage).every((item) => item === false)) {
+        setError(false);
+      }
+    } else {
+      isMounted.current = true;
+    }
+  }, [errorMessage]);
 
   async function signUp() {
     try {
@@ -42,7 +62,7 @@ export default function SignUpComponent({ user }) {
       const data = {
         UID: user.user.uid,
         'Login Email': signUpEmail,
-        'Organisation Type': type,
+        'Organisation Type': orgType,
         'Organisation Name': orgName,
         'Primary Contact Email': contactEmail,
         'Primary Contact Name': contactName,
@@ -62,64 +82,59 @@ export default function SignUpComponent({ user }) {
       return console.log(error);
     }
   }
-  console.log(errorMessage.length);
 
   function checkErrors() {
-    const emailReg = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,63}$';
+    setCounter((count) => (count += 1));
+    const emailRegex = new RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,63}$');
+    const phoneRegex = new RegExp(/^\d+$/);
+
+    if (orgType.length < 1) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, ORG_TYPE: true };
+      });
+    }
+
+    if (orgName.length < 1) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, ORG_NAME: true };
+      });
+    }
+
+    if (contactName.length < 1) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, CONTACT_NAME: true };
+      });
+    }
+
+    if (!emailRegex.test(contactEmail)) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, CONTACT_EMAIL: true };
+      });
+    }
+
+    if (!phoneRegex.test(contactNumber)) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, CONTACT_NUMBER: true };
+      });
+    }
+
+    if (!emailRegex.test(signUpEmail)) {
+      setError(true);
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, EMAIL: true };
+      });
+    }
 
     if (signUpPassword.length < 6) {
       setError(true);
-      setErrorMessage((errors) => {
-        return {
-          ...errors,
-          password: 'Password needs to be at least 6 characters',
-        };
+      setErrorMessage((errorObject) => {
+        return { ...errorObject, PASSWORD: true };
       });
-    }
-    if (!signUpEmail.match(emailReg)) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, email: 'Contact email is not valid' };
-      });
-    }
-
-    if (/^\d+$/.contactNumber == false) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, phone: 'Phone number is not valid' };
-      });
-    }
-
-    if (!contactEmail.match(emailReg)) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, contactEmail: 'Login email is not valid' };
-      });
-    }
-
-    if (!contactName.length > 0) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, contactName: 'Please enter your name' };
-      });
-    }
-
-    if (!orgName.length > 0) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, orgName: 'Please enter the organisation name' };
-      });
-    }
-
-    if (!type.length > 0) {
-      setError(true);
-      setErrorMessage((errors) => {
-        return { ...errors, orgType: 'Please select organisation type' };
-      });
-    }
-
-    if (errorMessage === undefined) {
-      return setError(false);
     }
   }
 
@@ -130,208 +145,198 @@ export default function SignUpComponent({ user }) {
           sx={{
             minWidth: 275,
             marginTop: 8,
+            marginBottom: 8,
             padding: 5,
             borderRadius: 3,
-
             mt: 5,
           }}
         >
           <form>
-            <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
-              Sign up
-            </Typography>
+            <Stack spacing={3}>
+              <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
+                Sign up
+              </Typography>
 
-            <Box sx={{ mb: 2 }}>
-              <FormControl fullWidth>
-                <InputLabel
+              <Box>
+                <FormControl fullWidth>
+                  <InputLabel
+                    required
+                    hidden
+                    id="organisation-type"
+                    htmlFor="Organisation Type"
+                  >
+                    Organisation Type
+                  </InputLabel>
+                  <Select
+                    required
+                    error={errorMessage.ORG_TYPE ? true : false}
+                    labelId="organisation-type-label"
+                    id="organisation-type"
+                    value={orgType}
+                    label="Organisation Type"
+                    onChange={(e) => {
+                      setOrgType(e.target.value);
+                      errorMessage.ORG_TYPE = false;
+                    }}
+                  >
+                    <MenuItem value={'School'}>School</MenuItem>
+                    <MenuItem value={'Retirement Home'}>
+                      Retirement Home
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+                {errorMessage.ORG_TYPE ? (
+                  <Error>Please select an organisation type</Error>
+                ) : null}
+              </Box>
+
+              <Box>
+                <label hidden htmlFor="Organisation Name">
+                  Organisation Name
+                </label>
+                <TextField
+                  error={errorMessage.ORG_NAME ? true : false}
                   required
-                  hidden
-                  id="organisation-type"
-                  htmlFor="Organisation Type"
-                >
-                  Organisation Type
-                </InputLabel>
-                <Select
-                  required
-                  error={errorMessage.orgType ? true : false}
-                  labelId="organisation-type-label"
-                  id="organisation-type"
-                  value={type}
-                  label="Organisation Type"
+                  variant="outlined"
+                  id="organisation-name-input"
+                  label="Organisation Name"
+                  autoComplete="current-organisation-name"
                   onChange={(e) => {
-                    setType(e.target.value);
-                    delete errorMessage.orgType;
+                    setOrgName(e.target.value);
+                    errorMessage.ORG_NAME = false;
                   }}
-                >
-                  <MenuItem value={'School'}>School</MenuItem>
-                  <MenuItem value={'Retirement Home'}>Retirement Home</MenuItem>
-                </Select>
-              </FormControl>
-              {errorMessage.orgType ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.orgType}
-                </Alert>
-              ) : null}
-            </Box>
+                  fullWidth
+                />
+                {errorMessage.ORG_NAME ? (
+                  <Error>Please enter an organisation name</Error>
+                ) : null}
+              </Box>
 
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="Organisation Name">
-                Organisation Name
-              </label>
-              <TextField
-                error={errorMessage.orgName ? true : false}
-                required
-                variant="outlined"
-                id="filled-organisation-name-input"
-                label="Organisation Name"
-                autoComplete="current-organisation-name"
-                onChange={(e) => {
-                  setOrgName(e.target.value);
-                  delete errorMessage.orgName;
-                }}
-                fullWidth
-              />
-              {errorMessage.orgName ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.orgName}
-                </Alert>
-              ) : null}
-            </Box>
+              <Box>
+                <label hidden htmlFor="Primary Contact Name">
+                  Primary Contact Name
+                </label>
+                <TextField
+                  required
+                  error={errorMessage.CONTACT_NAME ? true : false}
+                  variant="outlined"
+                  id="primary-contact-name-input"
+                  label="Primary Contact Name"
+                  autoComplete="current-primary-contact-name"
+                  onChange={(e) => {
+                    setContactName(e.target.value);
+                    errorMessage.CONTACT_NAME = false;
+                  }}
+                  fullWidth
+                />
+                {errorMessage.CONTACT_NAME ? (
+                  <Error>Please enter a contact name</Error>
+                ) : null}
+              </Box>
 
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="Primary Contact Name">
-                Primary Contact Name
-              </label>
-              <TextField
-                required
-                error={errorMessage.contactName ? true : false}
-                variant="outlined"
-                id="filled-primary-contact-name-input"
-                label="Primary Contact Name"
-                autoComplete="current-primary-contact-name"
-                onChange={(e) => {
-                  setContactName(e.target.value);
-                  delete errorMessage.contactName;
-                }}
-                fullWidth
-              />
-              {errorMessage.contactName ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.contactName}
-                </Alert>
-              ) : null}
-            </Box>
+              <Box>
+                <label hidden htmlFor="Organisation Address">
+                  Primary Contact Email
+                </label>
+                <TextField
+                  error={errorMessage.CONTACT_EMAIL ? true : false}
+                  required
+                  variant="outlined"
+                  id="organisation-email-input"
+                  label="Primary Contact Email"
+                  autoComplete="current-organisation-email"
+                  onChange={(e) => {
+                    setContactEmail(e.target.value);
+                    errorMessage.CONTACT_EMAIL = false;
+                  }}
+                  fullWidth
+                />
+                {errorMessage.CONTACT_EMAIL ? (
+                  <Error>Email is not valid</Error>
+                ) : null}
+              </Box>
 
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="Organisation Address">
-                Primary Contact Email
-              </label>
-              <TextField
-                error={errorMessage.contactEmail ? true : false}
-                required
-                variant="outlined"
-                id="filled-organisation-email-input"
-                label="Primary Contact Email"
-                autoComplete="current-organisation-email"
-                onChange={(e) => {
-                  setContactEmail(e.target.value);
-                  delete errorMessage.contactEmail;
-                }}
-                fullWidth
-              />
-              {errorMessage.contactEmail ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.contactEmail}
-                </Alert>
-              ) : null}
-            </Box>
+              <Box>
+                <label hidden htmlFor="Primary Contact Number">
+                  Primary Contact Number
+                </label>
+                <TextField
+                  required
+                  error={errorMessage.CONTACT_NUMBER ? true : false}
+                  variant="outlined"
+                  id="primary-contact-number-input"
+                  label="Primary Contact Number"
+                  autoComplete="current-primary-contact-number"
+                  onChange={(e) => {
+                    setContactNumber(e.target.value);
+                    errorMessage.CONTACT_NUMBER = false;
+                  }}
+                  fullWidth
+                />
+                {errorMessage.CONTACT_NUMBER ? (
+                  <Error>Phone number is not valid</Error>
+                ) : null}
+              </Box>
 
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="Primary Contact Number">
-                Primary Contact Number
-              </label>
-              <TextField
-                required
-                error={errorMessage.phone ? true : false}
-                variant="outlined"
-                id="filled-primary-contact-number-input"
-                label="Primary Contact Number"
-                autoComplete="current-primary-contact-number"
-                onChange={(e) => {
-                  setContactNumber(e.target.value);
-                  delete errorMessage.phone;
-                }}
-                fullWidth
-              />
-              {errorMessage.phone ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.phone}
-                </Alert>
-              ) : null}
-            </Box>
+              <Box>
+                <label hidden htmlFor="email">
+                  Login Email
+                </label>
+                <TextField
+                  required
+                  error={errorMessage.EMAIL ? true : false}
+                  variant="outlined"
+                  id="email-input"
+                  label="Login Email"
+                  type="email"
+                  autoComplete="current-email"
+                  fullWidth
+                  onChange={(e) => {
+                    setSignUpEmail(e.target.value);
+                    errorMessage.EMAIL = false;
+                  }}
+                />
+                {errorMessage.EMAIL ? (
+                  <Error>Login email is not valid </Error>
+                ) : null}
+              </Box>
 
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="email">
-                Login Email
-              </label>
-              <TextField
-                required
-                error={errorMessage.email ? true : false}
+              <Box>
+                <label hidden htmlFor="password">
+                  Password
+                </label>
+                <TextField
+                  required
+                  error={errorMessage.PASSWORD ? true : false}
+                  id="password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  fullWidth
+                  onChange={(e) => {
+                    setSignUpPassword(e.target.value);
+                    errorMessage.PASSWORD = false;
+                  }}
+                />
+                {errorMessage.PASSWORD ? (
+                  <Error>Password needs to be at least 6 characters</Error>
+                ) : null}
+              </Box>
+
+              <Button
+                fullWidth
                 variant="outlined"
-                id="filled-email-input"
-                label="Login Email"
-                type="email"
-                autoComplete="current-email"
-                onChange={(e) => {
-                  setSignUpEmail(e.target.value);
-                  delete errorMessage.email;
+                sx={{ padding: 1.85 }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  checkErrors();
+                  if (!error) return console.log('YAY');
+                  if (error) return console.log({ errorMessage });
                 }}
-                fullWidth
-              />
-              {errorMessage.email ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.email}
-                </Alert>
-              ) : null}
-            </Box>
-            <Box sx={{ mb: 2 }}>
-              <label hidden htmlFor="password">
-                Password
-              </label>
-              <TextField
-                required
-                error={errorMessage.password ? true : false}
-                id="filled-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                onChange={(e) => {
-                  setSignUpPassword(e.target.value);
-                  delete errorMessage.password;
-                }}
-                fullWidth
-              />
-              {errorMessage.password ? (
-                <Alert severity="error" sx={{ padding: 1.85, mt: 2 }}>
-                  {errorMessage.password}
-                </Alert>
-              ) : null}
-            </Box>
-            <Button
-              variant="outlined"
-              sx={{ padding: 1.85 }}
-              onClick={(e) => {
-                e.preventDefault();
-                checkErrors();
-                if (error === false) {
-                  signUp();
-                }
-                console.log('ERROR STATE', error);
-              }}
-              fullWidth
-            >
-              SIGN UP
-            </Button>
+              >
+                SIGN UP
+              </Button>
+            </Stack>
           </form>
         </Card>
       </Container>
