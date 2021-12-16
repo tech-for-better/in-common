3cd3d3e45bc5ac453a2a781b239102853f7cb215
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-16 10:52:43 +0000

    made the help page and help component

---
+++ components/Help/Help.js
@@ -0,0 +1,10 @@
+import { Container, Box, Alert } from '@mui/material';
+
+export default function Help({ user }) {
+  return (
+    <Container component="main" maxWidth="xs">
+      {' '}
+      Hello World
+    </Container>
+  );
+}
